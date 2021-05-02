19850b28f48bfd0a0c15eb1dd52b3c50f3f5e6e4
kouya takahashi <3kamarin4skaya6@gmail.com>
2021-05-02 12:01:22 +0900

    [add] albumentation flag

--- src/dataset.py
+++ src/dataset.py
@@ -12,11 +12,12 @@ from settings import CFG
 
 
 class Img_dataset(Dataset):
-    def __init__(self, paths, train_tran, val_tran):
+    def __init__(self, paths, train_tran, val_tran, albu=True):
         super().__init__()
         self.paths = paths
         self.train_tran = train_tran
         self.val_tran = val_tran
+        self.albu = albu
     
     def __len__(self):
         return len(self.paths)
@@ -25,9 +26,19 @@ class Img_dataset(Dataset):
         file_path = self.paths[idx]
         image = cv2.imread(file_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+        if self.albu:
+            image = self._get_aug()(image=image)['image']
 
         return (self.train_tran(image=image)['image'], 
                 self.val_tran(image=image)['image'])
+    
+    def _get_aug(self):
+        transform = [
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.5),
+            A.ChannelDropout(channel_drop_range=(1, 1), fill_value=0, p=0.5),
+            ]
+        return A.Compose(transform)
 
 
 def get_transform(*, TRAIN=False):
