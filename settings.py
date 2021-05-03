3c95f06839cdb11eff9a865cd2a041c7c96fff07
kouya takahashi <3kamarin4skaya6@gmail.com>
2021-05-03 13:52:29 +0900

    [add]

--- settings.py
+++ settings.py
@@ -13,22 +13,22 @@ line_token_path = os.getenv('LINE_TOKEN_PATH')
 class CFG:
     INPUT_DIR = os.getenv('INPUT_DIR')
     OUTPUT_DIR = os.getenv('OUTPUT_DIR')
-    filename = '1-2'
-    shrink_scale = 3
+    filename = 'high_2'
+    shrink_scale = 5
     ### 16:9
     # xsize =
     # ysize =
     ### 4:3
-    xsize = 720
-    ysize = 960
+    xsize = 120 
+    ysize = 160
     low_xsize = xsize // shrink_scale
     low_ysize = ysize // shrink_scale
-    batch_size = 2
-    epoch = 80
-    trained_param = f'{OUTPUT_DIR}/models/4-3_2/gen_070.pytorch'
-#    trained_param = False
+    batch_size = 32
+    epoch = 30
+    trained_param = f'{OUTPUT_DIR}/models/xxugen_070.pytorch'
+    trained_param = False
     IMG_DIR = f'{INPUT_DIR}/4-3/images/{filename}'
 
     OUTPUT_IMG = f'{OUTPUT_DIR}/images/4-3/{filename}'
-    OUTPUT_MODEL = f'{OUTPUT_DIR}/models/4-3'
+    OUTPUT_MODEL = f'{OUTPUT_DIR}/models/srcnn'
     LINE_TOKEN = get_line_token(line_token_path)
--- src/model.py
+++ src/model.py
@@ -1,7 +1,9 @@
 import torch
 from torch import nn
 
-
+class AbstractNet(nn.Module):
+    only_luminance = False
+    input_upscale = True
 
 
 class Generator(nn.Module):
@@ -68,3 +70,33 @@ class Discriminator(nn.Module):
     def forward(self, x):
         out = self.conv5(self.conv4(self.conv3(self.conv2(self.conv1(x)))))
         return self.out_patch(out)
+
+class SRCNN(AbstractNet):
+    """
+    SRCNN
+    ref: http://mmlab.ie.cuhk.edu.hk/projects/SRCNN.html
+    """
+
+    def __init__(self, **kwargs):
+        super(SRCNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=9, padding=4)
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=1, padding=0)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=3, kernel_size=5, padding=2)
+        self.activate = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        h = self.activate(self.conv1(x))
+        h = self.activate(self.conv2(h))
+        return self.conv3(h)
+
+
+MODELS = {
+        'srcnn' : SRCNN,
+        'pix2pix': Generator,
+        }
+
+def get_network(name):
+    for n, cls in MODELS.itens():
+        if n == name:
+            return cls
+    raise ValueError(f'Model {name} is not defined.')
