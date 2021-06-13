98626b948cd65817ec7edab03877c82e9e14f579
kouya takahashi <3kamarin4skaya6@gmail.com>
2021-06-13 17:09:37 +0900

    refactoring

--- src/model.py
+++ src/model.py
@@ -71,6 +71,7 @@ class Discriminator(nn.Module):
         out = self.conv5(self.conv4(self.conv3(self.conv2(self.conv1(x)))))
         return self.out_patch(out)
 
+
 class SRCNN(AbstractNet):
     """
     SRCNN
@@ -79,9 +80,18 @@ class SRCNN(AbstractNet):
 
     def __init__(self, **kwargs):
         super(SRCNN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=9, padding=4)
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=1, padding=0)
-        self.conv3 = nn.Conv2d(in_channels=32, out_channels=3, kernel_size=5, padding=2)
+        self.conv1 = nn.Conv2d(
+                in_channels=3, out_channels=64, 
+                kernel_size=9, padding=4
+                )
+        self.conv2 = nn.Conv2d(
+                in_channels=64, out_channels=32, 
+                kernel_size=1, padding=0
+                )
+        self.conv3 = nn.Conv2d(
+                in_channels=32, out_channels=3, 
+                kernel_size=5, padding=2
+                )
         self.activate = nn.ReLU(inplace=True)
 
     def forward(self, x):
