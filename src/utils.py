f93b9cb809cae0cdac5223e94722600fd171991a
kouya takahashi <3kamarin4skaya6@gmail.com>
2021-06-13 17:10:10 +0900

    [add] resize function

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,24 @@ import requests
 import json
 
 
+def calculate_original_img_size(origin_size: int, upscale_factor: int) -> int:
+    """
+    元の画像サイズを縮小拡大したいときに元の画像をどの大きさに
+    resize する必要があるかを返す関数
+    例えば 202 px の画像を 1/3 に縮小することは出来ない(i.e. 3の倍数ではない)ので
+    事前に 201 px に縮小しておく必要がありこの関数はその計算を行う
+    すなわち
+    calculate_original_img_size(202, 3) -> 201
+    となる
+
+    Args:
+        origin_size:
+        upscale_factor:
+    Returns:
+    """
+    return origin_size - (origin_size % upscale_factor)
+
+
 def show_image(path):
     img = cv2.imread(path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -17,6 +35,7 @@ def conv_scale01(arr):
     ma = arr.max()
     mi = arr.min()
     return (arr-mi) / (ma-mi)
+
 
 def send_line_message(token, text):
     '''
@@ -32,6 +51,7 @@ def get_line_token(token_path):
     with open(token_path, 'r') as f:
          token = json.load(f)['token']
     return token
+
 
 def resize_img(image, magnification=3):
     '''
