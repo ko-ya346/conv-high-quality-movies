1b568e3c3155afe9afdfc026a1abadb2fd0cddcc
kouya takahashi <3kamarin4skaya6@gmail.com>
2021-06-15 08:21:57 +0900

    add

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ import numpy as np
 
 import torch
 from torch import nn
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, DataLoader
 from tqdm.auto import tqdm
 from torch.optim.lr_scheduler import StepLR
 from torchvision.utils import save_image
@@ -14,10 +14,9 @@ from dotenv import load_dotenv
 
 
 sys.path.append('../')
-sys.path.append('./')
 
-from settings import CFG
-from src.dataset import get_dataloader, get_transform
+from config import CFG
+from src.dataset import TrainDataset
 from src.model import get_network 
 from src.utils import get_line_token, send_line_message
 
@@ -27,27 +26,57 @@ load_dotenv(dotenv_path)
 
 line_token = get_line_token(os.getenv('LINE_TOKEN_PATH'))
 
-def train():
+def train(debug=False):
+    max_epoch = CFG.epoch
+    if debug:
+        max_epoch = 1
+
     dataset_queries = {
-            '4-3': [
-                os.path.join(os.getenv('INPUT_DIR'), 
-                                f'images/{CFG.dataset}/*.png'),
-                os.path.join(os.getenv('INPUT_DIR'),
-                    f'images/{CFG.valid}/image-00*.png')
-                ],
+            'T91':  os.path.join(os.getenv('INPUT_DIR'), 'T91/*.png'),
+            'Set5':  os.path.join(os.getenv('INPUT_DIR'), 'Set5/*.png'),
+            'Set14':  os.path.join(os.getenv('INPUT_DIR'), 'Set14/*.png'),
             }
 
-    _, train_loader = get_dataloader(dataset_queries.get(CFG.aspect_ratio)[0], 
-        CFG.batch_size)
-    _, valid_loader = get_dataloader(dataset_queries.get(CFG.aspect_ratio)[1], 
-        CFG.batch_size)
+    
+    # train dataloader
+    train_query = dataset_queries.get(CFG.train_dataset)
+    train_dataset = TrainDataset(
+            train_query, max_size=CFG.max_size, 
+            shrink_scale=CFG.shrink_scale, 
+            total_samples=CFG.total_samples, 
+            input_upsample=CFG.input_upsample,
+            )
+    train_loader = DataLoader(
+            train_dataset, batch_size=CFG.batch_size, 
+            shuffle=True,
+            )
 
+    # valid dataloader
+    valid_query = dataset_queries.get(CFG.valid_dataset)
+    valid_dataset = TrainDataset(
+            valid_query, max_size=CFG.max_size, 
+            shrink_scale=CFG.shrink_scale, 
+            total_samples=CFG.total_samples, 
+            input_upsample=CFG.input_upsample,
+            )
+    valid_loader = DataLoader(
+            valid_dataset, batch_size=CFG.batch_size, 
+            shuffle=True,
+            )
+
+    # model
     model = get_network(CFG.model)()
     model = model.to(device)
 
-    optimizer = torch.optim.Adam(model.parameters(),
-                lr=0.01, weight_decay=1e-8)
-    scheduler = StepLR(optimizer, step_size=15, gamma=0.1)
+    # optimizer
+    optimizer = torch.optim.Adam(
+            model.parameters(),
+            lr=CFG.lr, 
+            weight_decay=CFG.weight_decay
+            )
+    scheduler = StepLR(
+            optimizer, step_size=CFG.lr_step, gamma=CFG.lr_gamma
+            )
     if os.path.exists(f'os.getenv("OUTPUT_DIR")/{CFG.model}'):
         states = torch.load(f'os.getenv("OUTPUT_DIR")/model/{CFG.model}_010.pytorch')
         model.load_state_dict(states)
@@ -59,9 +88,9 @@ def train():
     metrics = {}
     metrics["train"] = []
     metrics["valid"] = []
-    save_interval =5 
-    max_epoch = CFG.epoch
+    save_interval = 5 
 
+    # train loop
     for epoch in tqdm(range(max_epoch)):
         print(f'epoch: {epoch+1}')
         scheduler.step()
@@ -69,16 +98,13 @@ def train():
         
         epoch_loss = []
         
-        for num, (label, img_input) in enumerate(train_loader):
-            batch_len = len(label)
-            label /= 255
-            img_input /= 255
-            label, img_input = label.to(device), img_input.to(device)
+        for num, (img_input, img_target) in enumerate(train_loader):
+            img_input, img_target = img_input.to(device), img_target.to(device)
 
             output = model(img_input)
             output_tensor = output.detach()
 
-            loss = criterion(output, label)
+            loss = criterion(output, img_target)
 
             optimizer.zero_grad()
             loss.backward()
@@ -93,27 +119,33 @@ def train():
         if not os.path.exists(output_model):
             os.mkdir(output_model)
         if epoch % save_interval == 0 or epoch == max_epoch:
-            torch.save(model.state_dict(), f"{output_model}/{CFG.model}_{epoch:03}.pytorch")     
+            torch.save(
+                    model.state_dict(), 
+                    f"{output_model}/{CFG.model}_{epoch:03}.pytorch"
+                    )
 
-        # 生成画像を保存
-        save_image(output_tensor[:10], 
-                os.path.join(output_model, f'train_{epoch:02}_gen.png'))
-        save_image(label[:10], 
-                os.path.join(output_model, f'train_{epoch:02}_label.png'))
-
+        ### 画像を保存
+        # 低画質→高画質に変換した画像
+        save_image(
+                output_tensor[:10], 
+                os.path.join(output_model, f'train_{epoch:02}_gen.png')
+                )
+        # 元画像
+        save_image(
+                img_target[:10], 
+                os.path.join(output_model, f'train_{epoch:02}_target.png')
+                )
 
 
         # valid
         model.eval()
-        for label, img_input in valid_loader:
+        for img_input, img_target in valid_loader:
             valid_loss = []
 
-            label /= 255
-            img_input /= 255
-            label, img_input = label.to(device), img_input.to(device)
+            img_input, img_target = img_input.to(device), img_target.to(device)
 
             output = model(img_input)
-            loss = criterion(output, label)
+            loss = criterion(output, img_target)
             output_tensor = output.detach()
             valid_loss.append(loss.item())
         metrics['valid'].append(np.mean(valid_loss)) 
@@ -122,10 +154,14 @@ def train():
         print(f"valid: {metrics['valid'][-1]}")
         
         # 生成画像を保存
-        save_image(output_tensor[:10], 
-                os.path.join(output_model, f'valid_{epoch:02}_gen.png'))
-        save_image(label[:10], 
-                os.path.join(output_model, f'valid_{epoch:02}_label.png'))
+        save_image(
+                output_tensor[:10], 
+                os.path.join(output_model, f'valid_{epoch:02}_gen.png')
+                )
+        save_image(
+                img_target[:10], 
+                os.path.join(output_model, f'valid_{epoch:02}_target.png')
+                )
 
 
 if __name__ == '__main__':
