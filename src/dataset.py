6a0efcb13bed3a54dee5d5363eaae35aca33d9ff
kouya takahashi <3kamarin4skaya6@gmail.com>
2021-06-15 08:21:40 +0900

    add

--- src/dataset.py
+++ src/dataset.py
@@ -7,43 +7,42 @@ import numpy as np
 
 from torch.utils.data import DataLoader, Dataset
 
-from torchvision import transform as T
+from torchvision import transforms as T
 
-sys.path.append('../')
+from src.utils import calculate_original_img_size
+
 from config import CFG
-from utils import calculate_original_img_size
 
 
 
 class TrainDataset(Dataset):
-        """
-        Glob Query にマッチする画像からランダムに切り取られた正方形の画像と
-        それを縮小した画像のペアを返すデータセット
-        引数によって既に縮小された画像を返すかどうかが変わってくるので注意
-        Args:
-            query: glob query
-            max_size:
-                ランダムに切り取る画像の最大 pixel 数.
-                正確な値は shrink_scale とともに 
-                `calculate_original_img_size` によって計算される値になります.
-            shrink_scale:
-                切り取られた画像サイズを変換するスケール.
-                例えば 3 が設定されると, 切り取られた画像を 1/3 に縮小します.
-            total_samples:
-                1 epoch あたりの画像枚数. 
-                指定しないとすべての画像を一通り見ると 1epoch になります.
-                特定の値を設定すると, その数だけランダムにデータを 
-                pickup したときに 1epoch とします.
-            input_upsample:
-                True のとき入力画像を resize して target 画像と同じ大きさに前もって変換します.
-                変換方法は `interpolation` で指定されたアルゴリズムを使用します.
-            interpolation:
-        """
+    """
+    Glob Query にマッチする画像からランダムに切り取られた正方形の画像と
+    それを縮小した画像のペアを返すデータセット
+    引数によって既に縮小された画像を返すかどうかが変わってくるので注意
+    Args:
+        query: glob query
+        max_size:
+            ランダムに切り取る画像の最大 pixel 数.
+            正確な値は shrink_scale とともに 
+            `calculate_original_img_size` によって計算される値になります.
+        shrink_scale:
+            切り取られた画像サイズを変換するスケール.
+            例えば 3 が設定されると, 切り取られた画像を 1/3 に縮小します.
+        total_samples:
+            1 epoch あたりの画像枚数. 
+            指定しないとすべての画像を一通り見ると 1epoch になります.
+            特定の値を設定すると, その数だけランダムにデータを 
+            pickup したときに 1epoch とします.
+        input_upsample:
+            True のとき入力画像を resize して target 画像と同じ大きさに前もって変換します.
+            変換方法は `interpolation` で指定されたアルゴリズムを使用します.
+        interpolation:
+    """
     def __init__(
             self, query, max_size=128, shrink_scale=3,
             total_samples=10000, input_upsample=True,
-            interpolation=Image.BICUBIC,
-            ):
+            interpolation=Image.BICUBIC):
         super().__init__()
 
         self.img_paths = list(glob(query)) 
@@ -81,14 +80,24 @@ class TrainDataset(Dataset):
             T.RandomHorizontalFlip()
             ])
     
+        # max_sizeより画像が小さい場合は拡大する
+        self.upscale_transform = T.Compose([
+            T.Resize(size=max_size, interpolation=interpolation),
+            ])
+
     def __len__(self):
         return self.total_samples 
     
     def __getitem__(self, idx):
         file_path = self.img_paths[idx % self.n_images]
-        img = cv2.imread(file_path)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype(np.float32)
-        img = self.preprocess(img)
+        img = Image.open(file_path)
+        
+        try:
+            img = self.preprocess(img)
+        except:
+            img = self.upscale_transform(img)
+            img = self.preprocess(img)
+
 
         img_target = img.copy()
         img_target = self.target_transform(img_target)
@@ -99,23 +108,22 @@ class TrainDataset(Dataset):
     
 
 class TestDataset(Dataset):
-        """
-        Args:
-            query: 
-                glob query
-            xsize:
-                入力画像のx軸方向のサイズ
-            ysize:
-                入力画像のy軸方向のサイズ
-            up_scale:
-                画像を拡大するサイズ
-            interpolation:
-        """
+    """
+    Args:
+        query: 
+            glob query
+        xsize:
+            入力画像のx軸方向のサイズ
+        ysize:
+            入力画像のy軸方向のサイズ
+        up_scale:
+            画像を拡大するサイズ
+        interpolation:
+    """
     def __init__(
             self, query, xsize, ysize, 
             up_scale=3,
-            interpolation=Image.BICUBIC,
-            ):
+            interpolation=Image.BICUBIC):
         super().__init__()
 
         self.img_paths = list(glob(query)) 
@@ -138,8 +146,7 @@ class TestDataset(Dataset):
     
     def __getitem__(self, idx):
         file_path = self.img_paths[idx]
-        img = cv2.imread(file_path)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype(np.float32)
+        img = Image.open(file_path)
 
         img_input = self.input_transform(img)
 
