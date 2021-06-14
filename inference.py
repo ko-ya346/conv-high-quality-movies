8035d2e6c2d4830f393bd26fdffb912cbae92434
kouya takahashi <3kamarin4skaya6@gmail.com>
2021-06-15 08:21:04 +0900

    修正

--- inference.py
+++ inference.py
@@ -5,7 +5,7 @@ import numpy as np
 
 import torch
 from torch import nn
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, DataLoader
 from tqdm.auto import tqdm
 from torch.optim.lr_scheduler import StepLR
 import torchvision
@@ -16,8 +16,8 @@ from dotenv import load_dotenv
 sys.path.append('../')
 sys.path.append('./')
 
-from settings import CFG
-from src.dataset import get_dataloader, get_transform
+from config import CFG
+from src.dataset import TestDataset
 from src.model import get_network 
 from src.utils import get_line_token, send_line_message
 
@@ -28,38 +28,44 @@ load_dotenv(dotenv_path)
 line_token = get_line_token(os.getenv('LINE_TOKEN_PATH'))
 
 def inference():
-    dataset_queries = {
-            '4-3': [
-                os.path.join(os.getenv('INPUT_DIR'), 
-                                f'images/{CFG.dataset}/*.png'),
-                os.path.join(os.getenv('INPUT_DIR'),
-                    f'images/{CFG.valid}/*.png')
-                ],
-            }
+    query = os.path.join(os.getenv('INPUT_DIR'), 
+                         f'images/{CFG.inference_dataset}/*.png')
 
-    paths, valid_loader = get_dataloader(dataset_queries.get(CFG.aspect_ratio)[1], 
-        CFG.batch_size,
-        shuffle=False,
-        albu=False
-        )
-    save_name = [path.split('/')[-1] for path in paths]
+    # inference dataloader
+    inference_dataset = TestDataset(
+            query, xsize=CFG.xsize, 
+            ysize=CFG.ysize,
+            up_scale=CFG.up_scale,
+            )
+    inference_dataloader = DataLoader(
+            inference_dataset, 
+            batch_size=1,
+            shuffle=False,
+            )
+    save_name = [path.split('/')[-1] for path in list(glob(query))]
 
     model = get_network(CFG.model)()
     model = model.to(device)
 
-    states = torch.load(f'{os.getenv("OUTPUT_DIR")}/model/{CFG.model}_000.pytorch')
+    states = torch.load(f'{os.getenv("OUTPUT_DIR")}/model/{CFG.model}_295.pytorch')
     model.load_state_dict(states)
     
-    output_dir = os.path.join(os.getenv('OUTPUT_DIR'), 'images', CFG.valid)
+    output_dir = os.path.join(
+            os.getenv('OUTPUT_DIR'), 
+            'images', 
+            CFG.inference_dataset
+            )
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
         
-    for i, (image, _) in enumerate(tqdm(valid_loader)):
-        image /= 255
+    for i, image in enumerate(tqdm(inference_dataloader)):
         image = image.to(device)
         gen_image = model(image)
         gen_image_tensor = gen_image.detach()
-        save_image(gen_image_tensor, f'{output_dir}/{save_name[i]}')
+        save_image(
+                gen_image_tensor, 
+                f'{output_dir}/{save_name[i]}'
+                )
         
 
 if __name__ == '__main__':
