cbd8fee43a8d2cfb150d6b574a27885c137de893
kouya takahashi <3kamarin4skaya6@gmail.com>
2021-06-15 08:19:10 +0900

    入力fps追加

--- im2mo.sh
+++ im2mo.sh
@@ -1,9 +1,9 @@
 set filename $argv[1]
 set fps $argv[2]
 
-ffmpeg -i output/images/$filename/image-%05d.png -vcodec libx264 -r $fps output/movies/$filename.mp4
+ffmpeg -r $fps -i output/images/$filename/image-%05d.png -vcodec libx264 -r $fps output/movies/$filename.mp4
 
 ffmpeg -i input/movies/$filename.wmv -vn -acodec copy output/sound.wav
 ffmpeg -i output/movies/$filename.mp4 -i output/sound.wav -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 output/movies/aaa.mp4
-sudo rm output/sound.wav output/movies/$filename.mp4 -y
+sudo rm output/sound.wav output/movies/$filename.mp4
 sudo mv output/movies/aaa.mp4 output/movies/$filename.mp4
